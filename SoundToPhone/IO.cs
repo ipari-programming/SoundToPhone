7c0600f8f59631796637a70b6506b02baaa06721
Csáktornyai Ádám <jockahun@gmail.com>
2020-02-25 11:56:27 +0100

    DrawH added

--- SoundToPhone/IO.cs
+++ SoundToPhone/IO.cs
@@ -13,16 +13,20 @@ namespace SoundToPhone
 {
     public static class IO
     {
-        public static void LoadWav(string fileName)
+        public static List<byte> LoadWav(string fileName)
         {
+            byte[] buffer;
+
             using (WaveFileReader reader = new WaveFileReader(fileName))
             {
                 Assert.AreEqual(16, reader.WaveFormat.BitsPerSample, "Only works with 16 bit audio");
-                byte[] buffer = new byte[reader.Length];
+                buffer = new byte[reader.Length];
                 int read = reader.Read(buffer, 0, buffer.Length);
                 short[] sampleBuffer = new short[read / 2];
                 Buffer.BlockCopy(buffer, 0, sampleBuffer, 0, read);
             }
+
+            return buffer.ToList();
         }
     }
 }
--- SoundToPhone/Program.cs
+++ SoundToPhone/Program.cs
@@ -10,7 +10,33 @@ namespace SoundToPhone
     {
         static void Main(string[] args)
         {
-            IO.LoadWav("../../../telefon.wav");
+            List<byte> buffer = IO.LoadWav("../../../telefon.wav");
+            Console.Write("Wav loaded! Press any key to draw...");
+            Console.ReadKey();
+            DrawH(buffer);
+            Console.ReadKey();
+        }
+
+        static void DrawH(List<byte> buffer)
+        {
+            int unit = buffer.Count / Console.WindowWidth;
+            List<byte> currentSegment;
+
+            for (int i = 0; i < buffer.Count; i += unit)
+            {
+                if (i / unit + 1 >= Console.WindowWidth) break;
+
+                if (i + unit > buffer.Count) currentSegment = buffer.GetRange(i, buffer.Count - i);
+                else currentSegment = buffer.GetRange(i, unit);
+
+                Console.SetCursorPosition(i / unit, Console.WindowHeight - AvgByte(currentSegment) / 5);
+                Console.Write("#");
+            }
+        }
+
+        static byte AvgByte(List<byte> bytes)
+        {
+            return (byte)bytes.Average((byte input) => { return input; });
         }
     }
 }
