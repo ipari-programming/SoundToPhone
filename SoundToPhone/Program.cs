0d45f05ce001136012491b730cd33f6259f31286
Csáktornyai Ádám <jockahun@gmail.com>
2020-02-25 12:40:36 +0100

    Added noise reduction

--- SoundToPhone/Program.cs
+++ SoundToPhone/Program.cs
@@ -11,6 +11,7 @@ namespace SoundToPhone
         static void Main(string[] args)
         {
             List<byte> buffer = IO.LoadWav("../../../telefon.wav");
+            buffer = NoiseReduction(buffer, 200);
             Console.Write("Wav loaded! Press any key to draw...");
             Console.ReadKey();
             DrawH(buffer);
@@ -30,13 +31,25 @@ namespace SoundToPhone
                 else currentSegment = buffer.GetRange(i, unit);
 
                 Console.SetCursorPosition(i / unit, Console.WindowHeight - AvgByte(currentSegment) / 5);
-                Console.Write("#");
+                Console.Write(AvgByte(currentSegment) / 5);
             }
         }
 
         static byte AvgByte(List<byte> bytes)
         {
             return (byte)bytes.Average((byte input) => { return input; });
+        }
+
+        static List<byte> NoiseReduction(List<byte> bytes, byte thereshold)
+        {
+            List<byte> output = new List<byte>();
+
+            for (int i = 0; i < bytes.Count; i++)
+            {
+                output.Add(bytes[i] > thereshold ? bytes[i] : (byte)0);
+            }
+
+            return output;
         }
     }
 }
